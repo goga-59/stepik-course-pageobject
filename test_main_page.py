60f5303fb43f80409dd3a37ba74dc9faf2a14351
Egor <egsad2004@gmail.com>
2025-04-06 20:34:07 +0500

    change selector to a locator tuple of MainPageLocators, add checks to test case: test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 def test_guest_should_see_login_link(browser):
@@ -11,3 +12,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
