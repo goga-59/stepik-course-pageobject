2a5ebf052f385a961a5d0b84e1a5df8bc0f2ff1a
Egor <egsad2004@gmail.com>
2025-04-06 20:12:04 +0500

    Add is_element_present() to catch NoSuchElementException to BasePage and login link assertion to MainPage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage:
-    def __init__(self, browser, url):
+
+    def __init__(self, browser, url, timeout = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, by, value):
+        try:
+            self.browser.find_element(by, value)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
 from pages.main_page import MainPage
 
+def test_guest_should_see_login_link(browser):
+    link = "https://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 def test_guest_can_go_to_login_page(browser):
     link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
