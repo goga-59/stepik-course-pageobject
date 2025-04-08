a3d318cfa1c912c05f263725ddf5d8e6fb5fce16
Egor <egsad2004@gmail.com>
2025-04-08 20:38:06 +0500

    refactoring: add login methods to the BasePage, add login tests to the product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,8 @@ from math import sin, log
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from pages.locators import BasePageLocators
+
 
 class BasePage:
 
@@ -16,6 +18,13 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, by, value):
         try:
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 
 from conftest import browser
 from pages.locators import ProductPageLocators
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -47,3 +48,18 @@ def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)
     page.open()
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT), "Success message is displayed on product page, but should not be"
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
